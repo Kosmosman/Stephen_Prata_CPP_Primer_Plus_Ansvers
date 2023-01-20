cbfb29d2b509bc37afe045275cfad38c92217793
Joaquina Daeron <joaquind@un-k6.kzn.21-school.ru>
2023-01-20 11:10:03 +0300

    Add Chapter_4/Exercise_4.cpp

--- Chapter_6/Exercise_4.cpp
+++ Chapter_6/Exercise_4.cpp
@@ -20,45 +20,48 @@ int main() {
       {"Buddy Bob", "Trenee java programmer", "GoldenBoy", 0},
       {"Emmanuel Garylton", "Analyst", "FranchBoy", 2}};
 
-  cout << "Benevolent Order of Programmers Report" << endl;
-  cout << "a. display by name\tb. display by title" << endl;
-  cout << "c. display by bopname\td. display by preference" << endl;
-  cout << "q. quit" << endl;
+  if (massive) {
+    cout << "Benevolent Order of Programmers Report" << endl;
+    cout << "a. display by name\tb. display by title" << endl;
+    cout << "c. display by bopname\td. display by preference" << endl;
+    cout << "q. quit" << endl;
 
-  char ch = '\0';
-  cout << "Enter your choice: ";
-  while ((ch = cin.get()) != 'q') {
-    while (cin.get() != '\n') continue;
-    for (int i = 0; i < structsize; i++) {
-      switch (ch) {
-        case 'a':
-          cout << massive[i].fullname << endl;
-          break;
-        case 'b':
-          cout << massive[i].title << endl;
-          break;
-        case 'c':
-          cout << massive[i].bopname << endl;
-          break;
-        case 'd':
-          switch (massive[i].preference) {
-            case 0:
-              cout << massive[i].fullname << endl;
-              break;
-            case 1:
-              cout << massive[i].title << endl;
-              break;
-            case 2:
-              cout << massive[i].bopname << endl;
-              break;
-            default:
-              break;
-          }
-        default:
-          break;
-      }
-    }
+    char ch = '\0';
     cout << "Enter your choice: ";
+    while ((ch = cin.get()) != 'q') {
+      while (cin.get() != '\n') continue;
+      for (int i = 0; i < structsize; i++) {
+        switch (ch) {
+          case 'a':
+            cout << massive[i].fullname << endl;
+            break;
+          case 'b':
+            cout << massive[i].title << endl;
+            break;
+          case 'c':
+            cout << massive[i].bopname << endl;
+            break;
+          case 'd':
+            switch (massive[i].preference) {
+              case 0:
+                cout << massive[i].fullname << endl;
+                break;
+              case 1:
+                cout << massive[i].title << endl;
+                break;
+              case 2:
+                cout << massive[i].bopname << endl;
+                break;
+              default:
+                break;
+            }
+          default:
+            break;
+        }
+      }
+      cout << "Enter your choice: ";
+    }
+    delete[] massive;
   }
   return 0;
 }
