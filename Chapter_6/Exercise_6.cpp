fc0c3f2ea0747afe78726811447b8d5e51313614
Joaquina Daeron <joaquind@un-k6.kzn.21-school.ru>
2023-01-21 13:40:54 +0300

    Доделать Exercise_7.cpp

---
+++ Chapter_6/Exercise_6.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  using namespace std;
+  const int GrandDonation = 10000;
+
+  struct mecenat {
+    string name;
+    double sum;
+  };
+
+  int count = 0, num_top_mecenates = 0, top_mecenat_index = 0,
+      std_mecenat_index = 1;
+  bool error = false;
+  cout << "Enter the number of mecenatas: ";
+  (cin >> count).get();
+  mecenat* mecenates_list = new mecenat[count];
+  int* top_mecenates = new int[count]{-1};
+  if (mecenates_list && top_mecenates) {
+    for (int i = 0; i < count && !error; ++i) {
+      cout << "Enter the mecenat's name: ";
+      getline(cin, mecenates_list[i].name);
+      cout << "Enter the mecenat's donation: ";
+      (cin >> mecenates_list[i].sum).get();
+      if (mecenates_list[i].sum < 0) {
+        cout << "Error!";
+        error = true;
+      } else if (mecenates_list[i].sum >= GrandDonation) {
+        top_mecenates[top_mecenat_index] = i;
+        ++num_top_mecenates;
+        top_mecenat_index += 2;
+      } else {
+        top_mecenates[std_mecenat_index] = i;
+        std_mecenat_index += 2;
+      }
+    }
+    if (top_mecenates[0] >= 0 && top_mecenates[1] >= 0) {
+      cout << "Grand Patrons\n";
+      for (int i = 0; i <= num_top_mecenates; i += 2) {
+        cout << mecenates_list[top_mecenates[i]].name << endl;
+      }
+      cout << "\nPatrons\n";
+      for (int i = 1; i <= count - num_top_mecenates; i += 2) {
+        cout << mecenates_list[top_mecenates[i]].name << endl;
+      }
+    } else {
+      cout << "none";
+    }
+  }
+  delete[] mecenates_list;
+  delete[] top_mecenates;
+  return 0;
+}
---
+++ Chapter_6/Exercise_7.cpp
@@ -0,0 +1,23 @@
+#include <cstring>
+#include <iostream>
+
+int main() {
+  using namespace std;
+  const char* vovel = "aeiouyAEIOUY";
+  cout << "Enter words (q to exit): " << endl;
+  string word;
+  int count_vovel = 0, count_consonant = 0, count_other = 0;
+  while (cin >> word && word != "q") {
+    if (isalpha(word[0])) {
+      if (word.find_first_of(vovel, 0))
+        ++count_vovel;
+      else
+        ++count_consonant;
+    } else {
+      ++count_other;
+    }
+  }
+  cout << "Vovel - " << count_vovel << endl;
+  cout << "Consonant - " << count_consonant << endl;
+  cout << "Other - " << count_other << endl;
+}
